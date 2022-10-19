f1729c9ed8b418b691970e6f5685ee62cdf7853e
unknown <melih.gulec14@gmail.com>
2022-10-19 17:58:58 +0300

    Added react-native-sqlite-storage

--- android/app/src/main/java/com/noteapp/MainApplication.java
+++ android/app/src/main/java/com/noteapp/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.noteapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import org.pgsqlite.SQLitePluginPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
