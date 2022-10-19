8f307dd2cb20fcfc66d072f72a0ce888e5f2f8a2
unknown <melih.gulec14@gmail.com>
2022-10-19 17:29:46 +0300

    Created InputWithLabel component

---
+++ src/components/InputWithLabel/InputWithLabel.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Input from '../Input/Input';
+
+import styles from './InputWithLabel.style';
+
+const InputWithLabel = ({
+  label,
+  placeholder,
+  multiLine,
+  numberOfLines,
+  inputStyle,
+}) => {
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <Input
+        placeholder={placeholder}
+        multiLine={multiLine}
+        numberOfLines={numberOfLines}
+        style={multiLine && styles.inputStyle}
+      />
+    </View>
+  );
+};
+
+export default InputWithLabel;
---
+++ src/components/InputWithLabel/InputWithLabel.style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  label: {
+    marginBottom: 12,
+    fontSize: 16
+  },
+  inputStyle: {
+    textAlignVertical: 'top'
+  }
+});
