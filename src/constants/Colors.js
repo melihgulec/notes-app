bd3a8564c3f719c7ba2d9193a8fbf9a003f0e22f
unknown <melih.gulec14@gmail.com>
2022-10-23 19:02:40 +0300

    Added new colors to constants

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,7 +2,10 @@ const Colors = {
   black: '#000',
   white: '#fff',
   mainBlueC: '#3050fd',
-  grey: '#e0e0e0'
+  grey: '#f8f8f8',
+  darkGrey: '#c0c0c0',
+  red: '#F44336',
+  green: '#4CAF50',
 };
 
 export default Colors;
